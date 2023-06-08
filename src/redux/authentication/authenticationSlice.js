939136565da00fe8e810c9ec31caa3e58d5d0d6e
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-06-09 00:19:18 +0300

    fix authenticationSlice
     builder

--- src/redux/authentication/authenticationSlice.js
+++ src/redux/authentication/authenticationSlice.js
@@ -17,6 +17,7 @@ const initialState = {
   error: null,
 };
 
+/*
 export const authenticationSlice = createSlice({
   name: 'authentication',
   initialState,
@@ -97,5 +98,102 @@ export const authenticationSlice = createSlice({
     },
   },
 });
+*/
 
+export const authenticationSlice = createSlice({
+  name: 'authentication',
+  initialState,
+  reducers: {},
+  extraReducers:
+    builder => {
+    
+      builder.addCase(registerUser.pending, (state) => {
+         state.isLoading = true;
+         state.error = null;
+      });
+      
+      builder.addCase(registerUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = payload.token;
+        state.user = payload.user;
+        toast.success('Successfully registered!', {
+          theme: 'dark',
+        });
+      });
+
+      builder.addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again!', {
+          theme: 'dark',
+        });
+      });
+    
+      builder.addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      });
+
+    
+      builder.addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = payload.token;
+        state.user = payload.user;
+        toast.success('Successfully logged in!', {
+          theme: 'dark',
+        });
+      });
+      
+      builder.addCase(loginUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again!', {
+          theme: 'dark',
+        });
+      });
+    
+
+      builder.addCase(logoutUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      });
+  
+      builder.addCase(logoutUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isLoggedIn = false;
+        state.user = { name: null, email: null };
+        state.token = null;
+        toast.success('Successfully logged out! Waiting for you to come back!', {
+          theme: 'dark',
+        });
+      });
+    
+      builder.addCase(logoutUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again later!', {
+          theme: 'dark',
+        });
+      });
+  
+      builder.addCase(fetchCurrentUser.pending, (state) => {
+        state.isLoadingUser = true;
+        state.error = null;
+      });
+
+      builder.addCase(fetchCurrentUser.fulfilled, (state, { payload }) => {
+        state.isLoadingUser = false;
+        state.isLoggedIn = true;
+        state.user = payload;
+      });
+   
+      builder.addCase(fetchCurrentUser.rejected, (state, { payload }) => {
+        state.isLoadingUser = false;
+        state.error = payload;
+      });
+    }
+});
+    
 export const getAuthentication = state => state.authentication;
