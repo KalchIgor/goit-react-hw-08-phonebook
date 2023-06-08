969838f2b983158a1a2b0e2c6e6374b5868808c3
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-06-08 19:53:32 +0300

    NotFound

--- src/components/Application/PageNotFound/PageNotFound.js
+++ src/components/Application/PageNotFound/PageNotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import css from "./NotFound.module.css";
+import css from "./PageNotFound.module.css";
 import Img from "..//..//img/masha.gif";
 
 const PageNotFound = () => {
   return (
-    <div style={{ fontSize: '30px', textAlign: 'center' }}>
+    <div className={css.notFound} style={{ fontSize: '30px' }}>
       <p style={{ padding: '40px' }}>
         Ooops, Something went wrong:( Please, try again later! )
       </p>
-            <img src={Img} alt="poster"  />
+        <img src={Img} alt="poster"  />
     </div>
   );
 };
